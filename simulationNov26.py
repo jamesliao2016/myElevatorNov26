7a1cd27034cf8e374fa19ca9e1c712ec02f75a3f
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-11-26 11:23:36 +0800

    first2

--- simulationNov26.py
+++ simulationNov26.py
@@ -8,3 +8,11 @@ ppNumPerFloor = 300
 timePerFloor = 2
 timeOpenWait = 5
 
+floorAlocation = [(ii + 1) for ii in range(elevaNum)]
+
+def simRun(floorNum,elevaNum,ppNumPerFloor,
+           timeOpenWait,timePerFloor,floorAlocation):
+    for eleva_i in range(elevaNum):
+        floorPath = [(ii + 1) for ii in range(eleva_i)]
+        
+    
