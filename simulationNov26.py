0e9d12781c41fbcecdc8092f927117599a6cd837
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-11-26 11:41:00 +0800

    first3

--- simulationNov26.py
+++ simulationNov26.py
@@ -13,6 +13,9 @@ floorAlocation = [(ii + 1) for ii in range(elevaNum)]
 def simRun(floorNum,elevaNum,ppNumPerFloor,
            timeOpenWait,timePerFloor,floorAlocation):
     for eleva_i in range(elevaNum):
-        floorPath = [(ii + 1) for ii in range(eleva_i)]
-        
+        floorPathEleva_i = [(ii + 1) for ii in range(eleva_i)]
+        timeSpend = []
+        for floorEleva_i in floorPathEleva_i:
+            tmpTime = (timeOpenWait + timePerFloor) * floorEleva_i
+            timeSpend.append(tmpTime)
     
