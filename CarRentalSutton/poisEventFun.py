3cd7838612a7248717520309ed70c046bdfe1066
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-10 12:28:09 +0800

    dec 10, return poisson number

--- CarRentalSutton/poisEventFun.py
+++ CarRentalSutton/poisEventFun.py
@@ -13,6 +13,21 @@ def poisProbAcc(lambdax,nx):
         probAccRlt+=poisProbDen(lambdax,qq)
     return probAccRlt
 
+# Return the arrival number based on the poisson distribution
+def retPoiNum(lambdax):
+    jj = np.random.uniform()
+    rlt = 0.0
+    for kk in range(100):
+        dd1 = poisProbAcc(lambdax,kk)
+        dd2 = poisProbAcc(lambdax,(kk+1))
+
+        if jj > dd1 and jj < dd2:
+            rlt = kk
+            print(' %f %f %f' % (dd1, dd2, jj))
+            break
+    return rlt
+
+
 def eventOccur(lambdax,nx):
     jj = np.random.uniform()
     dd = poisProbAcc(3, 3)
@@ -22,13 +37,20 @@ def eventOccur(lambdax,nx):
     return rlt
 
 if __name__ == '__main__':
-    for cc in range(20):
-        jj = np.random.uniform()
-        dd1 = poisProbAcc(5,5)
-        dd2 = poisProbAcc(5, 3)
-        if jj < dd1 and jj > dd2:
-            print('got it %f %f'%(dd1,dd2))
-        else:
-            print('NO %f %f %f'%(dd1,dd2,jj))
+    # for cc in range(20):
+    #     jj = np.random.uniform()
+    #     dd1 = poisProbAcc(5,5)
+    #     dd2 = poisProbAcc(5, 3)
+    #     if jj < dd1 and jj > dd2:
+    #         print('got it %f %f'%(dd1,dd2))
+    #     else:
+    #         print('NO %f %f %f'%(dd1,dd2,jj))
 
     # print(poisEvent(2,2))
+    for cc in range(5):
+        print(retPoiNum(3))
+
+    # for jj in range(10):
+    #     print(jj)
+    #     if jj>5:
+    #         break
