6ff6d8a3a293b22a3e41d9317a586a4b283083f0
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-11 12:25:19 +0800

    dec 11, gridworld typo

--- gridSuttonDP/gridCh4Sutton.py
+++ gridSuttonDP/gridCh4Sutton.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-stNum = 5
+stNum = 8
 rewVal = -1
 prob = 0.5
-epsThr = 1e-3
+epsThr = 1e-6
 
 action = [0,1]
 states = list(range(1,stNum))
@@ -16,7 +16,7 @@ while True:
             dirct = 1
             if ee == 0:
                 dirct = -1
-            valVecTmp[qq] = prob * (rewVal + valVec[qq + dirct])
+            valVecTmp[qq] += prob * (rewVal + valVec[qq + dirct])
     gg = np.sum(np.abs(valVec -valVecTmp))
     if gg < epsThr:
         print(valVec)
