00d34fcbce6c0e4ef70e5ef78843bb21761c3507
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-11 12:23:55 +0800

    dec 11, gridworld evaluation

---
+++ gridSuttonDP/gridCh4Sutton.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+stNum = 5
+rewVal = -1
+prob = 0.5
+epsThr = 1e-3
+
+action = [0,1]
+states = list(range(1,stNum))
+valVec = np.zeros(stNum+1)
+
+while True:
+    valVecTmp = np.zeros(stNum+1)
+    for qq in states:
+        for ee in action:
+            dirct = 1
+            if ee == 0:
+                dirct = -1
+            valVecTmp[qq] = prob * (rewVal + valVec[qq + dirct])
+    gg = np.sum(np.abs(valVec -valVecTmp))
+    if gg < epsThr:
+        print(valVec)
+        break
+    valVec = valVecTmp
