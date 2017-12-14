b63b5744be28b126d36e7f0c7ed2c3a025251318
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-14 21:42:34 +0800

    dec 14, pic for gambler

--- gamblerSuttonDp/gamblerDP.py
+++ gamblerSuttonDp/gamblerDP.py
@@ -5,21 +5,16 @@ import numpy as np
 # The objective is to reach the two ends
 
 stNum = 100
-rewVal = -1
-prob = 0.5
-epsThr = 1e-6
+epsThr = 0.00000001
 probH = 0.4
 
-action = [0,1]
-states = list(range(1,stNum))
-valVec = np.zeros(stNum+1)
-
-stAct = [[(1.0 / (jj+1)) for ii in range(jj+1)] for jj in range(1,stNum)]
-stActRaw = [[(1.0 / (jj+1)) for ii in range(jj+1)] for jj in range(1,stNum)]
-
-states = list(range(1,stNum))
+states = list(range(0,stNum))
 valVec = np.zeros(stNum+1)
 valVec[stNum]=1.0
+
+stAct = [[(1.0 / (min((jj+1),(101 - jj)))) for ii in range(0,min((jj+1),(101 - jj)))]\
+         for jj in range(0,stNum)]
+
 # Function for value evaluation
 def funEval(states,valVec,stAct,epsThr,stNum):
     while True:
@@ -27,7 +22,7 @@ def funEval(states,valVec,stAct,epsThr,stNum):
         valVecTmp[stNum] = 1.0
         for qq in states:
             yy = 1
-            for ee in stAct[qq-1]:
+            for ee in stAct[qq]:
                 rewVal = yy - 1
                 futVal = probH * (valVec[min(qq + rewVal,stNum)]) \
                          + (1 - probH) * (valVec[max(0,qq - rewVal)])
@@ -43,37 +38,36 @@ def funEval(states,valVec,stAct,epsThr,stNum):
 
 valVec = funEval(states,valVec,stAct,epsThr,stNum)
 
+# stActRaw = [[(0) for ii in range(jj+1)] for jj in range(0,stNum)]
+
 # Function for value improvement
 def funImpr(states,valVec,stAct,epsThr,stNum):
     for qq in states:
         yy = 0
         optVal = -999999999999
         tmpProduct = 1.0
-        stActNu = stActRaw
-        if np.random.uniform() > 0.05:
-            stActNu = stAct
-        for ee in stActNu[qq-1]:
+        # stActNu = stAct
+        for ee in stAct[qq]:
             dirct = yy
             # valVecTmp[qq] += ee * (rewVal + valVec[qq + dirct])
-            tmpVal = probH * (valVec[min(stNum,qq + dirct)] - valVec[qq])\
-                     + (1 - probH) * (valVec[max(0,qq - dirct)] - valVec[qq])
+            tmpVal = probH * (valVec[min(stNum,qq + dirct)] )\
+                     + (1 - probH) * (valVec[max(0,qq - dirct)] )
             if tmpVal > optVal:
                 optVal = tmpVal
                 optSol = yy
-            yy +=1
+            yy += 1
             tmpProduct = min(ee,tmpProduct)
         if tmpProduct >= 0.00001:
             uu = 0.0
-            for yy in range(len(stAct[qq - 1])):
+            for yy in range(len(stAct[qq])):
                 # dltTmp = stAct[qq - 1][yy] - max(stAct[qq - 1][yy] - 0.01,0)
-                dltTmp = stAct[qq - 1][yy]
+                dltTmp = stAct[qq][yy]
                 # stAct[qq - 1][yy] = max(stAct[qq - 1][yy] - 0.01,0)
-                stAct[qq - 1][yy] = 0
+                stAct[qq][yy] = 0
                 uu += dltTmp
-                stAct[qq - 1][yy] = round(stAct[qq - 1][yy])
-
-            stAct[qq-1][optSol] = stAct[qq-1][optSol] + uu
-            stAct[qq - 1][optSol] = round(stAct[qq - 1][optSol])
+                stAct[qq][yy] = round(stAct[qq][yy])
+            stAct[qq][optSol] = stAct[qq][optSol] + uu
+            stAct[qq][optSol] = round(stAct[qq][optSol])
 
     return stAct
 
@@ -95,7 +89,6 @@ while True:
     stAct = stActTmp
 stActFinal = []
 
-
 for pp in stAct:
     aa = 0
     for ss in (pp):
@@ -103,5 +96,7 @@ for pp in stAct:
             stActFinal.append(aa)
         aa+=1
 import matplotlib.pyplot as plt
+print(len(states))
+print(len(stActFinal))
 plt.scatter(states,stActFinal)
 plt.show()
