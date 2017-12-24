26df85dbdad469c20c7fb80eff59fa1e64cd71f2
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-24 12:09:15 +0800

    dec 24, queueing with initial cars

--- CarRentalSutton/queueFun.py
+++ CarRentalSutton/queueFun.py
@@ -48,10 +48,11 @@ if __name__ == '__main__':
 
     lamx = 2.0
     mux = 2.0
-    iniCarNum = 10
+    iniCarNum = 20
     sampleSize = 1000
     timeHo = 20
     freqVec = transitFreq(iniCarNum,sampleSize,timeHo,lamx,mux)
     sumval = np.sum(freqVec)
-    freqVec2 = [round((ww / sumval),3) for ww in freqVec]
+    freqVec2 = [(round((ww / sumval),1)) for ww in freqVec]
+    freqVec2 = [(round(ww,1)) for ww in freqVec2]
     print(freqVec2)
