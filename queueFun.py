f81d195b3315d9206e1c047454b28fc9979d648f
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-24 11:41:06 +0800

    dec 24, queueing with initial cars

--- queueFun.py
+++ queueFun.py
@@ -16,23 +16,37 @@ for tt in range(horizon):
     gg.append(t1+t0)
     t0 += t1
 
+iniCarNum = 10
 sampleSize = 1000
 timeHo = 20
 thVal = lamx / (mux + lamx)
 freqVec = np.zeros(timeHo)
 for nn in range(sampleSize):
-    qNum = 0
-    sNum = 0
+    qNum = 0 # Number of queued people
+    sNumt = 0 # Number of served people
+    cNum = iniCarNum  # Number of present cars
     for qq in range(timeHo):
         mm = np.random.rand()
         if mm < thVal:
             # Event: new consumer arrives
-            qNum += 1
+            if cNum > 0:  # When there are cars available
+                sNum = min(qNum+1,cNum)
+                qNum = max(0,(qNum + 1 - sNum))
+                cNum = max(0,(cNum - sNum))
+            else:
+                qNum += 1
+                sNum = 0
         else:
-            if qNum>0:
-                sNum += 1
-                qNum -= 1
-    freqVec[sNum] += 1
+            # Event: new car returns
+            if qNum>0:  # When there are people are requesting cars
+                sNum = min(qNum, cNum + 1)
+                qNum = max(0, (qNum - sNum - 1))
+                cNum = max(0, (cNum + 1 - sNum))
+            else:
+                sNum = 0
+                cNum += 1
+        sNumt += sNum
+    freqVec[sNumt] += 1
 print(freqVec)
 sumval = np.sum(freqVec)
 freqVec2 = [round((ww / sumval),3) for ww in freqVec]
