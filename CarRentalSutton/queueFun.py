901b79ad6333bc33eb5aa51ae9c4dc9490a2ade1
jamesliao2016 <453143942@qq.com>
2017-12-25 09:41:31 +0800

    dec 25, queue matrix left served

--- CarRentalSutton/queueFun.py
+++ CarRentalSutton/queueFun.py
@@ -13,7 +13,7 @@ def arriveTime(lamx):
 def transitFreq(iniCarNum,sampleSize,timeHo,lamx,mux):
     thVal = lamx / (mux + lamx)
     freqVec = np.zeros(timeHo)  # Vector of people served
-    stateVec = np.zeros(timeHo)  # Vector of current cars / state
+
     for nn in range(sampleSize):
         qNum = 0 # Number of queued people
         sNumt = 0 # Number of served people
@@ -40,9 +40,43 @@ def transitFreq(iniCarNum,sampleSize,timeHo,lamx,mux):
                     cNum += 1
             sNumt += sNum
         freqVec[sNumt] += 1
-        stateVec[cNum - 1] += 1
+
     sumval = np.sum(freqVec)
     freqVec2 = [float(round((ww / sumval), 2)) for ww in freqVec]
+    return (freqVec2)
+
+def freqSerLeft(iniCarNum,sampleSize,timeHo,lamx,mux):
+    thVal = lamx / (mux + lamx)
+    freqVec = [list(np.zeros(timeHo)) for cc in range(timeHo)]  # Vector of people served
+    for nn in range(sampleSize):
+        qNum = 0 # Number of queued people
+        sNumt = 0 # Number of served people
+        cNum = iniCarNum  # Number of present cars
+        for qq in range(timeHo):
+            mm = np.random.rand()
+            if mm < thVal:
+                # Event: new consumer arrives
+                if cNum > 0:  # When there are cars available
+                    sNum = min(qNum+1,cNum)
+                    qNum = max(0,(qNum + 1 - sNum))
+                    cNum = max(0,(cNum - sNum))
+                else:
+                    qNum += 1
+                    sNum = 0
+            else:
+                # Event: new car returns
+                if qNum>0:  # When there are people are requesting cars
+                    sNum = min(qNum, cNum + 1)
+                    qNum = max(0, (qNum - sNum - 1))
+                    cNum = max(0, (cNum + 1 - sNum))
+                else:
+                    sNum = 0
+                    cNum += 1
+            sNumt += sNum
+        freqVec[sNumt][cNum] += 1
+
+    sumval = np.sum(freqVec)
+    freqVec2 = [[float(round((gg / sumval), 2)) for gg in ww] for ww in freqVec]
     return (freqVec2)
 
 def transitFull(sampleSize,timeHo,lamx,mux):
@@ -58,11 +92,46 @@ if __name__ == '__main__':
     timeHo = 20
     freqVec = transitFreq(iniCarNum,sampleSize,timeHo,lamx,mux)
     sumval = np.sum(freqVec)
-    freqVec2 = [float(round((ww / sumval),2)) for ww in freqVec]
+    # freqVec2 = [float(round((ww / sumval),2)) for ww in freqVec]
 
     # print(freqVec2)
     #
     # dd = [[transitFreq(ff,sampleSize,timeHo,lamx,mux)] for ff in range(timeHo)]
     # print(dd)
-    ff = transitFull(sampleSize,timeHo,lamx,mux)
-    print(ff)
+    # ff = transitFull(sampleSize,timeHo,lamx,mux)
+    # print(ff)
+    #
+    thVal = lamx / (mux + lamx)
+    freqVec = [list(np.zeros(timeHo)) for cc in range(timeHo)]  # Vector of people served
+
+    for nn in range(sampleSize):
+        qNum = 0 # Number of queued people
+        sNumt = 0 # Number of served people
+        cNum = iniCarNum  # Number of present cars
+        for qq in range(timeHo):
+            mm = np.random.rand()
+            if mm < thVal:
+                # Event: new consumer arrives
+                if cNum > 0:  # When there are cars available
+                    sNum = min(qNum+1,cNum)
+                    qNum = max(0,(qNum + 1 - sNum))
+                    cNum = max(0,(cNum - sNum))
+                else:
+                    qNum += 1
+                    sNum = 0
+            else:
+                # Event: new car returns
+                if qNum>0:  # When there are people are requesting cars
+                    sNum = min(qNum, cNum + 1)
+                    qNum = max(0, (qNum - sNum - 1))
+                    cNum = max(0, (cNum + 1 - sNum))
+                else:
+                    sNum = 0
+                    cNum += 1
+            sNumt += sNum
+        freqVec[sNumt][cNum] += 1
+        # stateVec[cNum - 1] += 1
+    sumval = np.sum(freqVec)
+    freqVec2 = [[float(round((gg / sumval), 3)) for gg in ww] for ww in freqVec]
+    print(freqVec2)
+    print(np.sum(freqVec2))
